437732caade6627e7e5663d55db9ccdb820dd40d
binary-greens <arpanriju2000@gmail.com>
2020-07-13 17:37:10 +0530

    The complete application

---
+++ src/components/newTodo.vue
@@ -0,0 +1,30 @@
+<template>
+    <form @submit.prevent="addTodo()" class="col-12 col-lf-6">
+        <label>
+            <input v-model="newTodo" placeholder="Enter Todo" type="text" class="font-control">
+        </label>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "NewTodo",
+        data () {
+            return {
+                newTodo: ""
+            }
+        },
+        methods: {
+            addTodo(){
+                if(this.newTodo.length > 1) {
+                    this.$emit("on-addTodo", this.newTodo)
+                }
+                this.newTodo = ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
